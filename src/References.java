b0785133a50877a1bc9f4225e1a327a0564eecc8
reset <plonkh.fm@gmx.de>
2017-04-05 16:25:40 +0200

    Changed mutation chances for better results BIG IMPROVEMENT!

--- src/References.java
+++ src/References.java
@@ -2,11 +2,11 @@
  * Created by Niklas on 26.01.2016.
  */
 public class References {
-    public static double    numberMutationChance        = 0.5;
-    public static double    operatorMutationChance      = 0.1;
-    public static double    lengthMutationChance        = 0.1;
-    public static int       generationSize              = 2;
+    public static double    numberMutationChance        = 0.05;
+    public static double    operatorMutationChance      = 0.01;
+    public static double    lengthMutationChance        = 0.01;
+    public static int       generationSize              = 20000;
     public static int       defaultNumberChainLength    = 2;
-    public static double    aim                         = 213;
+    public static double    aim                         = 123456789;
     public static int       maxGenAmount                = 1000;
 }
